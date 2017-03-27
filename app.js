82090788ead73b6dd6663bd48fb83aa1f6e6a514
jinghai <jinghai.xiao@gmail.com>
2017-03-27 13:17:19 +0800

    delay

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var ut = require('./common.js');
 var async = require('async');
-console.log('开始测试!!!')
-var public_num = '支付宝';
+console.log('开始!!!');
+//要确保按关键字关键字搜素出的结果排在第一位
+//搜素地址:http://weixin.sogou.com/weixin 输入关键字，然后点“搜素众号”。。。
+var keyworld = '上海发布';//[上海发布、三亚政务、青海政务]
 //任务数组
 var task = [];
 //根据public_num搜索公众号,最好是微信号或者微信全名.
 task.push(function (callback) {
-  ut.search_wechat(public_num, callback)
+  ut.search_wechat(keyworld, callback)
 });
 //根据url获取公众号获取最后10条图文列表
 task.push(function (url, callback) {
