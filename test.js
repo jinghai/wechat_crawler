c6b10da3587407b205927bceb46200f5c27c46f3
jinghai <jinghai.xiao@gmail.com>
2017-03-30 19:50:52 +0800

    注意事项

--- test.js
+++ test.js
@@ -1,46 +1,41 @@
-/**
- * Created by Administrator on 2017/3/27.
- */
-var request = require('request');
-var cheerio = require('cheerio');
-
-//request.debug = true;
-
-var encode_public_num = encodeURIComponent("上海发布");
-var u = `http://weixin.sogou.com/weixin?type=1&query=${encode_public_num}&ie=utf8&_sug_=y&_sug_type_=1`;
-
-
-var FileCookieStore = require("tough-cookie-filestore");
-var jar = request.jar(new FileCookieStore("./cookie.json"));
-var headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:50.0) Gecko/20100101 Firefox/50.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-    //'Accept-Encoding': 'gzip, deflate',
-    'Accept-Encoding': 'gzip',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-}
-var request = request.defaults({
-    headers:headers,
-    jar: jar,
-    gzip:true,
-    /*proxy:'http://proxy.dc.lan:1234',*/
-    /*encoding : null,*/
+var ut = require('./common.js');
+var async = require('async');
+//var saveDatas = require('./model');
+console.log('开始!!!');
+//要确保按关键字关键字搜素出的结果排在第一位
+//搜素地址:http://weixin.sogou.com/weixin 输入关键字，然后点“搜素众号”。。。
+var keyworld = '青海政务';//[上海发布、三亚政务、青海政务]
+//任务数组
+var task = [];
+//根据public_num搜索公众号,最好是微信号或者微信全名.
+task.push(function (callback) {
+    ut.search_wechat(keyworld, callback)
+});
+//根据url获取公众号获取最后10条图文列表
+task.push(function (url, callback) {
+    ut.look_wechat_by_url(url, callback)
 });
 
-var url = "http://mp.weixin.qq.com/profile?src=3&timestamp=1490603427&ver=1&signature=WJrm-DvNBw04WoeJpGkrCYFTZxkAAdYDsuTf7tP2g6tW6pvK6uMFzn8XOK*wXftc0*WLSaRlHV2EEMWycpAf8A==";
-url = 'http://mp.weixin.qq.com/s?__biz=MjM5NTA5NzYyMA==&mid=2654009223&idx=3&sn=84979e97bdd09f4779d3d308608e896d&scene=0#wechat_redirect';
-request(url, function (err, response, html) {
-    if (err) {
-        console.log("err:",err);
-        return;
+var i = 0;
+
+async.whilst(
+    function() {
+        i++;
+        console.log(new Date(),'循环次数',i);
+        return true
+    },
+    function(cb) {
+        //执行任务
+        async.waterfall(task, function (err, result) {
+            if (err) {
+                console.error(err);
+                //process.exit(0);
+            }
+            setTimeout(function(){
+                cb();
+            },2*60*60*1000 /*30*1000*/);
+
+        })
     }
-    var $ = cheerio.load(html);
-    var text = $('#page-content').text();
+);
 
-    console.log(text);
-    //var t = iconv.decode(html, "utf8");
-    //console.log(t);
-
-})
